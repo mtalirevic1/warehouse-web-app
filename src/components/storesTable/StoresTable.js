97828f5efc10312804b6f369563999c7b43a4929
hajdinovic <haris.ajdinovic@hotmail.com>
2020-04-05 22:47:58 +0200

    omoguceno prebacivanje proizvoda iz skladista u poslovnice

--- src/components/storesTable/StoresTable.js
+++ src/components/storesTable/StoresTable.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Table,Layout, Modal} from 'antd';
+import {Table,Layout, Modal, AutoComplete, Form, Input, message} from 'antd';
 import axios from 'axios';
 
 import './StoresTable.css';
@@ -51,6 +51,20 @@ export default class StoresTable extends Component {
                             </div>
                         ) : null,
                 },
+                {
+                    title: 'Transfer products to store',
+                    dataIndex: 'transferProducts',
+                    render: (text, record) =>
+                        this.state.warehouseProducts ? (
+                            <div>
+                                <a key={record.id}
+                                    onClick={() => this.onOpenTransferProductsModal(record.id)}>Transfer products to store</a>
+                                <div>
+                                    {this.renderTransferProductsModal(record)}
+                                </div>
+                            </div>
+                        ) : null,
+                },
 
             ],
             columnsProducts: [
@@ -70,9 +84,26 @@ export default class StoresTable extends Component {
             ],
             isLoading: false,
             modalVisible: false,
+            transferModalVisible: false,
             activeItemId: null,
+            warehouseProducts: [],
+            quantityOfSelectedProduct: 0,
+            addQuantity: 0,
+            productId: null,
+            
         };
+        this.handleSelect = this.handleSelect.bind(this);
+        this.handleQuantityChange = this.handleQuantityChange.bind(this);
+        this.handleTransferProducts = this.handleTransferProducts.bind(this);
     }
+
+    handleQuantityChange(event) {
+        this.setState({ addQuantity: event.target.value });
+    }
+
+    handleSelect (quantity, id) { 
+        this.setState({quantityOfSelectedProduct: quantity, productId: id});
+      }
 
     onOpenModal = id => {
         this.setState({ modalVisible: true, activeItemId: id });
@@ -81,6 +112,14 @@ export default class StoresTable extends Component {
 
     onCloseModal = () => {
         this.setState({ modalVisible: false, activeItemId: null });
+    };
+
+    onOpenTransferProductsModal = id => {
+        this.setState({ transferModalVisible: true, activeItemId: id });
+    };
+
+    onCloseTransferProductsModal = () => {
+        this.setState({ transferModalVisible: false, activeItemId: null });
     };
 
     componentDidMount() {
@@ -92,7 +131,10 @@ export default class StoresTable extends Component {
             .then(response => {
                 console.log(response);
                 this.setState({ offices : response.data});
-            })
+            });
+        axios.get('https://main-server-si.herokuapp.com/api/warehouse')
+            .then(response => {
+                    this.setState({ warehouseProducts: response.data, isLoading: false })});
     }
 
     seeProducts = id => {
@@ -134,6 +176,107 @@ export default class StoresTable extends Component {
         }
     };
 
+    renderTransferProductsModal = record => {
+        var options = [];
+        this.state.warehouseProducts.forEach ( item => {
+            options.push({
+                value: item.productName,
+                id: item.productId,
+                quantity: item.quantity
+            });
+        });
+        if (this.state.activeItemId === record.id) {
+            return (
+                <Modal
+                    mask={false}
+                    title="Transfer products to store"
+                    centered
+                    visible = {this.state.transferModalVisible}
+                    onCancel = {() => this.onCloseTransferProductsModal()}
+                    onOk = {() => this.handleTransferProducts(this.state.activeItemId, this.state.addQuantity, this.state.quantityOfSelectedProduct)}
+                    okText="Transfer"
+                >
+                    <div>
+                        Product: 
+                        {<AutoComplete
+                            style = {{ width: 300 }}
+                            options = {options}
+                            placeholder = "Enter product name"
+                            filterOption = {(inputValue, option) =>
+                                option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+                            }
+                            onSelect ={this.onProductSelected}
+                            
+                        />}
+                    </div>
+                    {
+                        this.state.quantityOfSelectedProduct !== 0 ?
+                            <p>
+                                Quantity of selected item in warehouse: {this.state.quantityOfSelectedProduct}
+                            </p>
+                        : null
+                    }
+                    <Form>
+                    <Form.Item
+                        name="productQuantity"
+                        rules={[
+                            {
+                                required: true,
+                                message: "Please enter the quantity!",
+
+                            },
+                        ]}
+                    >
+                        <p>Qunatity of products to transfer: </p>
+                        <Input
+                            value={this.state.addQuantity} onChange={this.handleQuantityChange}
+                            className="modal-form-input"
+                            id="number"
+                            type="number"
+                            max={this.state.quantityOfSelectedProduct}
+                            min={0}
+                            placeholder="quantity"
+                        />
+                    </Form.Item>
+                </Form>
+                </Modal >
+            );
+        }
+    };
+
+    onProductSelected = (value, option) => {
+        this.handleSelect(option.quantity, option.id)
+    }
+
+    handleTransferProducts(storeId, quantityToAdd, maxAllowed) {
+        if(quantityToAdd > maxAllowed || quantityToAdd < 0){
+            message.error("Input not in allowed range");
+            return;
+        }
+        let token = 'Bearer ' + this.props.token;
+        axios.defaults.headers.common["Authorization"] = token;
+        axios.defaults.headers.common["Content-Type"] = "application/json";
+        console.log(storeId, this.state.productId, quantityToAdd);
+        var that = this;
+        axios.post('https://main-server-si.herokuapp.com/api/warehouse', {productId: this.state.productId, quantity: -quantityToAdd})
+            .then( () => {
+                that.state.warehouseProducts = that.state.warehouseProducts.filter(function(item) {
+                                                                                        return item.quantity !== 0;
+                                                                                    });
+                var length = that.state.warehouseProducts.length;
+                for (var i= 0; i < length; i++) {
+                    if(that.state.warehouseProducts[i].productId == that.state.productId)
+                        that.state.warehouseProducts[i].quantity-=quantityToAdd;
+                }
+                axios.post('https://main-server-si.herokuapp.com/api/inventory', { officeId: storeId, productId: that.state.productId, quantity: quantityToAdd}).then( () => {
+                    message.success("Successfuly transfered products");
+                    this.handleSelect(null, null);
+                    that.setState({addQunatity: null, transferModalVisible: false, activeItemId: null});
+                })
+            });
+         
+        
+    }
     render() {
         const dataSource = this.state.offices;
         const columns = this.state.columns;
